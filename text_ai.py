e4bf555b12a740a79f9c2b8995b5974151f8cde1
GALLARYWOULT <GALLARYWOULT@GMAIL.COM>
2025-04-29 10:35:58 +0530

    Add files via upload

--- text_ai.py
+++ text_ai.py
@@ -1,7 +1,2 @@
-from transformers import pipeline
-
-summarizer = pipeline("summarization")
-
-def summarize_text(text: str):
-    summary = summarizer(text, max_length=100, min_length=25, do_sample=False)
-    return summary[0]['summary_text']
+def summarize_text(text):
+    return text[:100] + "..." if len(text) > 100 else text
